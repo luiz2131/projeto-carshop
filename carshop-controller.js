9869f920d43e1f96225f97e53f10c6e9c76ee33e
luiz2131 <luizfelipeal2014@gmail.com>
2018-11-24 13:04:07 -0200

    projeto carshop aula

---
+++ carshop-controller.js
@@ -0,0 +1,96 @@
+class CarroController{
+    constructor(){
+       
+        this.carros = []; 
+        this.carregarAnuncio();
+    }
+
+    carregarAnuncio(){
+
+        var carrosLocalStorage = 
+        JSON.parse(window.localStorage.getItem("carro"));
+        
+        if(carrosLocalStorage != null){
+            this.carros = carrosLocalStorage;
+        }else{
+            this.carros = carroJson;
+        }        
+        for (let i = 0; i < this.carros.length; i++) {
+            this.anunciarCarro(this.carros[i],this.carro);
+        
+        }
+
+        
+    }
+
+   
+    anunciarCarro(carro){
+
+        var div = document.createElement("div");
+        div.className="carro";
+
+        var img = document.createElement("img");
+        img.src = carro.foto;
+
+        div.appendChild(img);
+
+
+        var fabricante = document.createElement("span");
+        fabricante.className = "fabricante";
+        fabricante.innerText = carro.fabricante;
+        div.appendChild(fabricante);
+
+        var nome = document.createElement("span");
+        nome.className = "nome";
+        nome.innerText = carro.nome;
+        div.appendChild(nome);
+
+        var divAnoKm = document.createElement("div");
+        divAnoKm.className = "anoKm";
+        var anoFabricacao = document.createElement("span");
+        anoFabricacao.className="ano";
+        anoFabricacao.innerText = "Ano " + carro.anoFabricacao;
+        divAnoKm.appendChild(anoFabricacao);
+
+        
+        var km = document.createElement("span");
+        km.innerText = carro.km + " KM";
+        km.className="km";
+        divAnoKm.appendChild(km);
+        div.appendChild(divAnoKm);
+
+        var divCorPorta = document.createElement("div");
+        divCorPorta.className = "CorPorta";
+
+        var cor = document.createElement("span");
+        cor.className="cor";
+        cor.innerText = "Cor: " + carro.cor;
+        divCorPorta.appendChild(cor);
+
+        var quantidadePortas = document.createElement("span");
+        quantidadePortas.innerText = carro.quantidadePortas + " Portas";
+        quantidadePortas.className="porta";
+
+        divCorPorta.appendChild(quantidadePortas);
+        div.appendChild(divCorPorta);
+
+
+        var divPreco = document.createElement("div");
+        divPreco.className="preco";
+
+
+        var preco = document.createElement("span");
+        preco.innerText = "R$: "+ carro.preco;
+        preco.className="preco";
+        divPreco.appendChild(preco);
+        div.appendChild(divPreco);
+        
+      
+
+
+        document.getElementById("conteudo").appendChild(div);
+
+    }
+}
+
+var carroController = new CarroController();
---
+++ carshop.js
@@ -0,0 +1,144 @@
+var carroJson = [
+    {
+		"nome":"Voyage",
+        "fabricante": "Volskwagen",
+        "cor":"Prata",
+        "anoFabricacao":"2018",
+        "tipo":"Carro",
+        "km": 0,
+        "quantidadePortas": 4,
+        "preco":"38.990",
+        "foto":"https://www.carpixel.net/w/2f0583047c1870a80fd7360cf39025ac/volkswagen-voyage-wallpaper-hd-57861.jpg",
+    },
+    {
+		"nome":"Fiesta",
+        "fabricante": "Ford",
+        "cor":"Branco",
+        "anoFabricacao":"2018",
+        "tipo":"Carro",
+        "km": 0,
+        "quantidadePortas": 4,
+     "preco":"65.000",
+     "foto":"https://res.cloudinary.com/carsguide/image/upload/f_auto,fl_lossy,q_auto,t_cg_hero_large/v1/editorial/Ford-Fiesta-vignale-white-2018-%286%29.jpg",
+    },{
+    "nome":"S10",
+    "fabricante":"Chevrolet",
+    "cor":"Prata",
+    "anoFabricacao":"2018",
+    "tipo":"Carro",
+    "km": 0,
+    "quantidadePortas": 4,
+    "preco":"180.000",
+    "foto":"https://i.ytimg.com/vi/kztFHTHwtxw/maxresdefault.jpg"
+    },{
+    "nome":"Corolla XRS ",
+    "fabricante":"Toyota",
+    "cor":"Preto",
+    "anoFabricacao":"2018",
+    "tipo":"Carro",
+    "km": 0,
+    "quantidadePortas": 4,
+    "preco":"120.000",
+    "foto":"https://img.olx.com.br/images/26/261814027373343.jpg",
+    },{
+    "nome":"500 Abarth ",
+    "fabricante":"Fiat",
+    "cor":"Vermelho",
+    "anoFabricacao":"2018",
+    "tipo":"Carro",
+    "km": 0,
+    "quantidadePortas": 4,
+    "preco":"38.950",
+    "foto":"http://www.dezeroacem.com.br/wordpress/wp-content/uploads/2014/11/Fiat-500-Abarth-2015.jpg",
+    },{
+        "nome":"Amarok ",
+        "fabricante":"Volskwagen ",
+        "cor":"Preto",
+        "anoFabricacao":"2018",
+        "tipo":"Carro",
+        "km": 0,
+        "quantidadePortas": 4,
+        "preco":"180.000",
+        "foto":"https://abrilquatrorodas.files.wordpress.com/2018/08/volkswagen-amarok-carlex-amy-4-e1535409100703.jpg?quality=70&strip=info&strip=info",
+
+    },{
+        "nome":"Sentra ",
+        "fabricante":"Nissan ",
+        "cor":"Branco",
+        "anoFabricacao":"2018",
+        "tipo":"Carro",
+        "km": 0,
+        "quantidadePortas": 4,
+        "preco":"110.000",
+        "foto":"https://c4d709dd302a2586107d-f8305d22c3db1fdd6f8607b49e47a10c.ssl.cf1.rackcdn.com/thumbnails/stock-images/0c39eaa395808648776576d82d0687d5.png",
+
+
+
+
+    },
+    {
+        "nome":"Versa ",
+        "fabricante":"Nissan ",
+        "cor":"prata",
+        "anoFabricacao":"2018",
+        "tipo":"Carro",
+        "km": 0,
+        "quantidadePortas": 4,
+        "preco":"120.900",
+        "foto":"https://images.dealer.com/ddc/vehicles/2018/Nissan/Versa/Sedan/color/Brilliant%20Silver-K23-188,188,188-640-en_US.jpg",
+
+
+
+
+    },
+    {
+        "nome":"Eclipse Release",
+        "fabricante":"Mitsubishi",
+        "cor":"Vermelho",
+        "anoFabricacao":"2018",
+        "tipo":"Carro",
+        "km": 0,
+        "quantidadePortas":2,
+        "preco":"210.000",
+        "foto":"https://i.pinimg.com/originals/e0/8b/7a/e08b7aa4d209b1a089bfc197eb94b6a7.jpg",
+
+
+
+
+    },
+    
+
+
+
+
+
+
+    
+ 
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
